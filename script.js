f5a5f0396acc0cca578eb02833ae63e8a365b633
DavidBotina09 <davidbotinasc@gmail.com>
2022-07-21 15:47:10 -0500

    terminando el ciclo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let acumulador = 0
+/*let acumulador = 0
 let numeroNotas =parseFloat(prompt("ingrese el numero de notas que desea promediar(maximo 10 notas)"))
 if(numeroNotas <= 10){
 
@@ -10,7 +10,39 @@ if(numeroNotas <= 10){
         }
     }
     console.log(`el promedio de las notas es ${acumulador / numeroNotas}`)
-}
+}*/
+
+let valorBoleta = 5000
+let clientes = 0
+let edad = 0
+do{
+    do{
+        clientes = parseInt(prompt("cuantos clientes van a ver la pelicula"))
+        if(isNaN(clientes)){
+            alert("ingresa caracteres validos")
+        }
+        if(clientes > 5){
+            alert("el maximo de clientes por grupo es de 5 personas")
+        }
+        console.log(clientes)
+    }while((isNaN(clientes)) || (clientes > 5))
+
+
+    for(let i = 0;i < clientes; i ++ ){
+            edad = parseInt(prompt("edad de cada cliente"))
+        if(isNaN(edad)){
+            alert("vuelve a ingresar un caracter validos")
+            break
+        }
+        if(edad < 17){
+            alert("la edad estimada para ver las peliculas es de 17 años en adelante")
+            break
+        }
+        console.log(edad)
+    }
+}while((edad < 17) || (isNaN(edad)))
+
+alert(`el total a pagar es ${valorBoleta * clientes}`)
 
 
 
